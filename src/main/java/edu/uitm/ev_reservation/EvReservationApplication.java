8a5b185b975dd858df1c9da21ae3085fda3a9c6e
Harith Iqbal <harith.iqbal@swiftlogistics.com.my>
2025-05-25 11:10:16 +0800

    initial commit

---
+++ src/main/java/edu/uitm/ev_reservation/EvReservationApplication.java
@@ -0,0 +1,13 @@
+package edu.uitm.ev_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uitm/ev_reservation/EvReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uitm.ev_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
