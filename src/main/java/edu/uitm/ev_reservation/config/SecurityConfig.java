952328f759c20fa8ee4bbf4da079c755470b7570
Harith Iqbal <harith.iqbal@swiftlogistics.com.my>
2025-06-10 14:29:10 +0800

    build: remove security temp

--- src/main/java/edu/uitm/ev_reservation/config/SecurityConfig.java
+++ src/main/java/edu/uitm/ev_reservation/config/SecurityConfig.java
@@ -26,10 +26,11 @@ public class SecurityConfig {
         .cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/auth/**").permitAll()
-            .requestMatchers("/api/health").permitAll()
-            .requestMatchers("/ws/**").permitAll()
-            .requestMatchers("/", "/index.html", "/static/**", "/assets/**", "/*.js", "/*.css", "/*.ico", "/*.svg", "/*.png", "/*.jpg", "/*.jpeg").permitAll()
+            // .requestMatchers("/api/auth/**").permitAll()
+            // .requestMatchers("/api/health").permitAll()
+            // .requestMatchers("/ws/**").permitAll()
+            // .requestMatchers("/", "/index.html", "/static/**", "/assets/**", "/*.js", "/*.css", "/*.ico", "/*.svg", "/*.png", "/*.jpg", "/*.jpeg").permitAll()
+            .requestMatchers("*").permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(sessionAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
